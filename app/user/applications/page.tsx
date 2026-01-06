5b3802616e1ab83c2909c675ec29e1c11a8acf13
Shreenath Panchal <shree.d.panchal1999@gmail.com>
2026-01-06 14:33:56 +0530

    base API Alpha

--- app/user/applications/page.tsx
+++ app/user/applications/page.tsx
@@ -208,7 +208,7 @@ export default function MyApplicationsPage() {
       const currentLimit = limit || pagination.limit;
 
       const response = await fetch(
-        `https://api.quikkred.in/api/application/loan/get?page=${currentPage}&limit=${currentLimit}`,
+        `https://alpha.quikkred.in/api/application/loan/get?page=${currentPage}&limit=${currentLimit}`,
         {
           method: 'GET',
           headers: {
