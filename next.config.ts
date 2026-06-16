6b380beb44c96dc1bd7b816f839d67f6385ad31b
Shreenath Panchal <shree.d.panchal1999@gmail.com>
2026-06-16 11:24:38 +0530

    Update Content Security Policy to include Facebook domain for script sources

--- next.config.ts
+++ next.config.ts
@@ -153,7 +153,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
             default-src 'self';
-            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://googleads.g.doubleclick.net https://connect.facebook.net https://cdn.jsdelivr.net https://checkout.razorpay.com;
+            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://googleads.g.doubleclick.net https://connect.facebook.net https://www.facebook.com https://cdn.jsdelivr.net https://checkout.razorpay.com;
             style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
             img-src 'self' data: https: blob:;
             font-src 'self' data: https://fonts.gstatic.com;
